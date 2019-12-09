d06558b30b9d35bb452af891166906469797c640
pelop <pelop@RayDark>
2019-12-09 12:00:40 +0100

    Creamos el metodo numerico

---
+++ NetBeansProjects/basicosSergio/src/basicossergio/BasicosSergioLarrion.java
@@ -0,0 +1,11 @@
+package basicossergio;
+
+import static basicossergio.numeros.numericos;
+
+public class BasicosSergioLarrion {
+
+public static void main(String[] args) {
+        numericos();  // Llamada al método
+    }
+    
+}
